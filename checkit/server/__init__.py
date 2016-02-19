1906bcc1caae8568b5eebcf6a8310580f4927223
Guillermo Carrasco <guille.ch.88@gmail.com>
2016-02-19 12:55:27 +0100

    Adapted app to run in heroku

--- checkit/server/__init__.py
+++ checkit/server/__init__.py
@@ -4,12 +4,13 @@ import os
 from checkit.backend import setup_stores
 from checkit.server.api import app
 
+
+"""Start and configure Flask application"""
+DB_URI = os.environ.get('DB_URI', 'sqlite:////tmp/checkit_db.db')
+setup_stores(DB_URI)
+
+app.config['GITHUB_SECRET'] = os.environ.get('GITHUB_SECRET')
+app.config['GITHUB_CLIENT'] = os.environ.get('GITHUB_CLIENT')
+
 def start_app():
-    """Start and configure Flask application"""
-    DB_URI = os.environ.get('DB_URI', 'sqlite:////tmp/checkit_db.db')
-    setup_stores(DB_URI)
-
-    app.config['GITHUB_SECRET'] = os.environ.get('GITHUB_SECRET')
-    app.config['GITHUB_CLIENT'] = os.environ.get('GITHUB_CLIENT')
-
     app.run(debug=True)
